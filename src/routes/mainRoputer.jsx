377903925db3303579bc945fa0d29b394c27c163
Marco <za19012397@zapopan.tecmm.edu.mx>
2025-12-13 18:42:34 -0600

    feat: Se implemento el router de la aplicacion y la proteccion de rutas

--- src/routes/mainRoputer.jsx
+++ src/routes/mainRoputer.jsx
@@ -1,5 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Login } from "../pages/login/login";
+import { ProtectedRoute } from "./protectedRoute";
+import { AppRouter } from "./appRouter";
 
 
 export function MainRouter(){
@@ -9,6 +11,10 @@ export function MainRouter(){
             <Routes>
                 <Route path="/" element={<Login />} />
                 <Route path="/login" element={<Login />} />
+
+                <Route element={<ProtectedRoute />}>
+                    <Route path="/*" element={<AppRouter />} />
+                </Route>
             </Routes>
         </BrowserRouter>
     )
