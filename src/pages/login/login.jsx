ce1643a06a5233cb8deae7d11c4e52d6d1d0c1fb
Marco <za19012397@zapopan.tecmm.edu.mx>
2025-12-13 18:31:35 -0600

    feat: Se implemento la funcion para logear a un usuario

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -4,6 +4,7 @@ import { Btn } from "../../ui/btn/btn";
 import { Card } from "../../ui/card/card";
 import { TextField } from "../../ui/textfield/textField";
 import { useState } from "react";
+import { apiFetch } from "../../api/api";
 
 export function Login() {
 
@@ -17,6 +18,33 @@ export function Login() {
     const handleChange = (event) => {
         const {name, value} = event.target
         setFormData({...formData, [name]: value})
+    }
+
+    const hanleSubmit = async()=>{
+        try{
+
+            const response = await apiFetch('users/login',{
+                method: 'POST',
+                body: JSON.stringify(formData)
+            })
+
+            let data = await response.json()
+
+            if(data.token){
+
+                const sessionData = {
+                    id: data.id,
+                    userName: data.user,
+                    fullName: `${data.firstName} ${data.lastName}`,
+                    token: data.token
+                }
+                localStorage.setItem('session', JSON.stringify(sessionData))
+
+            }
+
+        }catch(error){
+            console.log(error)
+        }
     }
 
     return (
@@ -43,7 +71,10 @@ export function Login() {
                 </DivCol>
                 
                 <DivCol>
-                    <Btn title='Iniciar sesion' />
+                    <Btn  
+                        title='Iniciar sesion' 
+                        handle={hanleSubmit} 
+                    />
                 </DivCol>
 
             </Card>
