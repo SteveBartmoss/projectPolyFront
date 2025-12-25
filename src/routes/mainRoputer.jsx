4e6ed2c73b37367eaa5397186f4814e85968b470
Marco <za19012397@zapopan.tecmm.edu.mx>
2025-12-24 19:48:16 -0600

    feat: Se implemento la ruta para el registro de usuarios

--- src/routes/mainRoputer.jsx
+++ src/routes/mainRoputer.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Login } from "../pages/login/login";
 import { ProtectedRoute } from "./protectedRoute";
 import { AppRouter } from "./appRouter";
+import { Register } from "../pages/register/register";
 
 
 export function MainRouter(){
@@ -11,6 +12,7 @@ export function MainRouter(){
             <Routes>
                 <Route path="/" element={<Login />} />
                 <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />}/>
 
                 <Route element={<ProtectedRoute />}>
                     <Route path="/*" element={<AppRouter />} />
