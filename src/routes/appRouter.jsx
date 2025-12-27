40efee65a95581c41c6e91337e9a13274af50fcb
Marco <za19012397@zapopan.tecmm.edu.mx>
2025-12-27 11:17:10 -0600

    feat: Se implemento la ruta para el profile del usuario

--- src/routes/appRouter.jsx
+++ src/routes/appRouter.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import { Home } from "../pages/home/home";
 import { Layout } from "../components/layout";
+import { Profile } from "../pages/profile/profile";
 
 
 export function AppRouter() {
@@ -9,6 +10,7 @@ export function AppRouter() {
         <Layout>
             <Routes>
                 <Route path="home" element={<Home />} />
+                <Route path="profile" element={<Profile />} />
             </Routes>
         </Layout>
     )
