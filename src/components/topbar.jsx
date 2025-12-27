326fc8c3bdf8f6d875795ac50173017c871cac35
Marco <za19012397@zapopan.tecmm.edu.mx>
2025-12-27 10:33:35 -0600

    feat: Se implemento el componente para el topbar

---
+++ src/components/topbar.jsx
@@ -0,0 +1,10 @@
+import './topbar.css'
+
+export function TopBar() {
+    return (
+        <div className='div-topbar'>
+            <h1>The pipe</h1>
+            <h1>User img</h1>
+        </div>
+    )
+}
