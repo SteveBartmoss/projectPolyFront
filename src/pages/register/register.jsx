cc81ca08086803dfb8e946493592e5ade67bb082
Marco <za19012397@zapopan.tecmm.edu.mx>
2025-12-26 22:33:42 -0600

    feat: Se mejoro el disenio de la pagina de registro

--- src/pages/register/register.jsx
+++ src/pages/register/register.jsx
@@ -1,88 +1,90 @@
 import { useState } from "react";
 import { DivCol } from "../../ui/boxes/boxes";
-import { Card } from "../../ui/card/card";
+import { Card, CardBody, CardFooter, CardHeader } from "../../ui/card/card";
 import { TextField } from "../../ui/textfield/textField";
 import { Btn } from "../../ui/btn/btn";
+import { apiFetch } from "../../api/api";
+import { AppLink } from "../../ui/applink/AppLink";
 
 
-export function Register(){
+export function Register() {
 
     const [formData, setFormData] = useState({
         user: "",
-		password: "",
-		firstName: "",
-		lastName: "",
+        password: "",
+        firstName: "",
+        lastName: "",
     })
 
     const handleChange = (event) => {
-        const {name,value} = event.target
-        setFormData({...formData, [name]: value})
+        const { name, value } = event.target
+        setFormData({ ...formData, [name]: value })
     }
 
     const handleSubmit = async () => {
-        try{
+        try {
 
-            const response = await apiFetch('users',{
+            const response = await apiFetch('users', {
                 method: 'POST',
                 body: JSON.stringify(formData)
             })
 
             let data = await response.json()
-            
-        }catch(error){
+
+            console.log(data)
+
+        } catch (error) {
             console.log(error)
         }
     }
 
-    return(
+    return (
         <>
-            <DivCol>
-                <Card>
+            <Card>
+
+                <CardHeader>
                     <h1>Crea tu cuenta hoy</h1>
+                </CardHeader>
+                
+                <CardBody>
+                    <TextField
+                        nameField="user"
+                        textHolder="Usuario"
+                        target={formData.user}
+                        handleTarget={handleChange}
+                    />
 
-                    <DivCol>
-                        <TextField 
-                            nameField="user" 
-                            textHolder="Usuario"
-                            target={formData.user}
-                            handleTarget={handleChange}
-                        />
-                    </DivCol>
+                    <TextField
+                        nameField="password"
+                        textHolder="Password"
+                        target={formData.password}
+                        handleTarget={handleChange}
+                    />
 
-                    <DivCol>
-                        <TextField
-                            nameField="password" 
-                            textHolder="Password"
-                            target={formData.password}
-                            handleTarget={handleChange} 
-                        />
-                    </DivCol>
+                    <TextField
+                        nameField="firstName"
+                        textHolder="Nombre"
+                        target={formData.firstName}
+                        handleTarget={handleChange}
+                    />
 
-                    <DivCol>
-                        <TextField
-                            nameField="firstName" 
-                            textHolder="Nombre"
-                            target={formData.firstName}
-                            handleTarget={handleChange} 
-                        />
-                    </DivCol>
-                    
-                    <DivCol>
-                        <TextField 
-                            nameField="lastName" 
-                            textHolder="Apellido"
-                            target={formData.lastName}
-                            handleTarget={handleChange}  
-                        />
-                    </DivCol>
+                    <TextField
+                        nameField="lastName"
+                        textHolder="Apellido"
+                        target={formData.lastName}
+                        handleTarget={handleChange}
+                    />
 
-                    <DivCol>
-                        <Btn
-                            title='Crear cuenta'
-                        />
-                    </DivCol>
-                </Card>
-            </DivCol>
+                    <Btn
+                        title='Crear cuenta'
+                        handle={handleSubmit}
+                    />
+                </CardBody>
+
+                <CardFooter>
+                    <p>Ya tienes una cuenta, inicia sesion <AppLink url={"/login"}>Aqui</AppLink> </p>
+                </CardFooter>
+            </Card>
         </>
     )
 }
