db25c06ceb09cc2fa52a6cb76b4724556c7bec13
Tony <za19012397@zapopan.tecmm.edu.mx>
2026-07-10 18:29:11 -0600

    feat: Se implementa la logica para que se cierre el modal del menu si se hace click en otras areas

--- src/ui/popover/popOver.jsx
+++ src/ui/popover/popOver.jsx
@@ -1,9 +1,10 @@
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import './popover.css'
 
-export function PopOver({ children, text }) {
+export function PopOver({ children, head=null }) {
 
     const [open, setOpen] = useState(false)
+    const menuRef = useRef(null)
 
     const handelOpen = () => {
         setOpen(!open)
@@ -13,14 +14,32 @@ export function PopOver({ children, text }) {
         setOpen(false)
     }
 
+    useEffect(()=> {
+
+        const handleCliclOutside = (e) => {
+            if(menuRef.current && !menuRef.current.contains(e.target)){
+                handleClose()
+            }
+        }
+
+        if(open) {
+            document.addEventListener('mousedown',handleCliclOutside)
+        }
+
+        return () => {
+            document.removeEventListener('mousedown',handleCliclOutside)
+        }
+        
+    },[open])
+
     return (
-        <div>
+        <div className='display-menu' ref={menuRef}>
             <div onClick={handelOpen}>
-                <p>{text}</p>
+                {head}
             </div>
             {
                 open && (
-                    <div>
+                    <div className='container-list'>
                         {children}
                     </div>
                 )
