e2e2f5c240ad4cbb78005d9d33d73cdb11a49632
Tony <za19012397@zapopan.tecmm.edu.mx>
2026-07-24 18:55:26 -0600

    feat: Se implementa un borrador de la libreria de componentes ui para la web

--- src/ui/textfield/textField.jsx
+++
@@ -1,7 +0,0 @@
-import './textfield.css'
-
-export function TextField({nameField,isPass=false,textHolder,target,handleTarget}){
-    return(
-        <input type={isPass ? "password" : "text"} name={nameField}  className='text-field' placeholder={textHolder} value={target} onChange={handleTarget} />
-    )
-}
