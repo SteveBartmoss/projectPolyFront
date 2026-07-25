7a06459505dae684877abaad82866c2c452f61f0
Tony <za19012397@zapopan.tecmm.edu.mx>
2026-07-24 19:08:15 -0600

    feat: Se implemento componentes simples para mostar la posibilidad de crear un post:

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,7 +1,49 @@
+import { useState } from "react";
+import { TextBox } from "../../plank-jsx/textBox/textBox";
+import { TextField } from "../../plank-jsx/textfield/textField";
+import { Btn } from "../../ui/btn/btn";
+
 
 
 export function Home(){
+
+    const [content, setContent] = useState('')
+    const [title, setTitle] = useState('')
+
     return(
-        <h1>Home</h1>
+        <>
+            <div style={{
+                width: "70%",
+                margin: "auto",
+            }}>
+
+                <div style={{
+                    width: "40rem",
+                    margin: "auto",
+                }}>
+                    <div style={{
+                        margin: "0.5rem",
+                        padding: "0.2rem"
+                    }}>
+                        <TextField textHolder={'title'} value={title} onChange={setTitle} />
+                    </div>
+                    <div style={{
+                        margin: "0.5rem",
+                        padding: "0.2rem"
+                    }}>
+                        <TextBox placeholder={'Content'} content={content} setContent={setContent} />
+                    </div>
+                    
+                    <div style={{
+                        margin: "0.5rem",
+                        padding: "0.2rem"
+                    }}>
+                        <Btn title="Publicar" />
+                    </div>
+                </div>
+
+            </div>
+            
+        </>
     )
 }
