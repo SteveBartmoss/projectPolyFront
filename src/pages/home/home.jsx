8a435d0a449384cbeb50620bf15fd541448cda7a
Tony <za19012397@zapopan.tecmm.edu.mx>
2026-08-01 19:12:24 -0600

    feat: Se implementan los componentes para el timeline

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -3,37 +3,13 @@ import { TextBox } from "../../plank-jsx/textBox/textBox";
 import { TextField } from "../../plank-jsx/textfield/textField";
 import { Btn } from "../../ui/btn/btn";
 import { apiClient } from "../../api/api";
-import { Timeline } from "./timeline";
+import { Timeline } from "../../components/timeline/timeline";
+import { TimelineForm } from "../../components/timeline/timelineForm";
 
 
 
 export function Home(){
 
-    const [content, setContent] = useState('')
-
-    const handleSubmit = async () => {
-        try {
-
-            const userData = JSON.parse(localStorage.getItem('session'))
-
-            console.log(userData)
-
-            const bodyContent = {
-                userId: userData.id,
-                content: content
-            }
-
-            console.log(bodyContent)
-
-            const response = await apiClient.post('/user-timeline/',{body: bodyContent})
-
-            console.log(response)
-
-        } catch (error) {
-            console.log(error)
-        }
-
-    }
     
     return(
         <>
@@ -42,24 +18,7 @@ export function Home(){
                 margin: "auto",
             }}>
 
-                <div style={{
-                    width: "40rem",
-                    margin: "auto",
-                }}>
-                    <div style={{
-                        margin: "0.5rem",
-                        padding: "0.2rem"
-                    }}>
-                        <TextBox placeholder={'Content'} content={content} setContent={setContent} />
-                    </div>
-                    
-                    <div style={{
-                        margin: "0.5rem",
-                        padding: "0.2rem"
-                    }}>
-                        <Btn title="Publicar" handle={handleSubmit} />
-                    </div>
-                </div>
+                <TimelineForm />
 
                 <Timeline />
                 
