2237b64a8041a3995555a8f30b116224e2195af3
Tony <89054896+SteveBartmoss@users.noreply.github.com>
2026-08-07 14:10:10 -0600

    feat: Se inicializa el componente para mostrar el time line de los post

---
+++ src/components/post/feedPost.jsx
@@ -0,0 +1,19 @@
+
+
+export function FeedPost(){
+
+    return(
+        <>
+            <Box styles={{
+                width: "40rem",
+                margin: "auto",
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                justifyContent: "center",
+            }}>
+                <h1>Post feed</h1>
+            </Box>
+        </>
+    )
+}
