ec101949cb25361d15079cc0d1220901749a2512
Tony <za19012397@zapopan.tecmm.edu.mx>
2026-08-06 18:44:13 -0600

    feat: Se implemento la ruta para la pagina de configuracion de la cuenta

--- src/routes/appRouter.jsx
+++ src/routes/appRouter.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import { Home } from "../pages/home/home";
 import { Layout } from "../components/layout";
 import { Profile } from "../pages/profile/profile";
+import { ConfigProfile } from "../pages/configProfile/configProfile";
 
 
 export function AppRouter() {
@@ -11,6 +12,7 @@ export function AppRouter() {
             <Routes>
                 <Route path="home" element={<Home />} />
                 <Route path="profile" element={<Profile />} />
+                <Route path="config-profile" element={<ConfigProfile />} />
             </Routes>
         </Layout>
     )
