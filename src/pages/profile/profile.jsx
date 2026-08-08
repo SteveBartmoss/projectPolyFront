30e82bee5345120a1cf4818e478f56942b69589d
Tony <za19012397@zapopan.tecmm.edu.mx>
2026-08-07 19:11:06 -0600

    feat: Se implemento la mejora para obtener el profile del usuario desde el backend

--- src/pages/profile/profile.jsx
+++ src/pages/profile/profile.jsx
@@ -5,8 +5,35 @@ import { Timeline } from '../../components/timeline/timeline'
 import { TimelineForm } from '../../components/timeline/timelineForm'
 import { Box } from '../../plank-jsx/containers/box'
 import './profile.css'
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { apiClient } from '../../api/api'
 
 export function Profile() {
+
+    const { id } = useParams()
+
+    const [profile, setProfile] = useState(null)
+
+    const getProfile = async () => {
+
+        try {
+
+            const response = await apiClient.get(`/profile-user/${id}`)
+            
+            setProfile(response.data)
+            console.log(response)
+
+        } catch (error) {
+            console.log(error)
+        }
+
+    }
+
+    useEffect(() => {
+        getProfile()
+    },[])
+
     return (
         <>
             <Box styles={{
@@ -23,14 +50,17 @@ export function Profile() {
                     <img className='profile-img' src={profileImg} />
                     <div>
                         <h1>Usuario</h1>
-                        <span>Nombre</span>
+                        <span>{profile.alias}</span>
+                    </div>
+                    <div>
+                        <p>{profile.biografy}</p>
                     </div>
                 </Box>
                 <Box styles={{
                     width: "70%",
                     margin: "auto",
                 }}>
-                    
+
                     <div className="profile-portada-container">
                         <img
                             className="profile-portada"
